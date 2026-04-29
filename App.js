16633e8db367f73e18c79685d505da705421f145
LucasRafa22 <lucassolimene22@gmail.com>
2026-04-29 10:12:20 -0300

    feat: add navigation app

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./src/screens/home";
+import CadastroPet from "./src/screens/CadastroPet";
+import Lembretes from "./src/screens/Lembretes";
+import ListaPets from "./src/screens/ListaPets";
+import Perfil from "./src/screens/Perfil";
+
 export default function App() {
   return (
-    <></>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Home name="Home" component={Home}/>
+        <Stack.Home name="CadastroPet" component={CadastroPet}/>
+        <Stack.Home name="Lembretes" component={Lembretes}/>
+        <Stack.Home name="ListaPets" component={ListaPets}/>
+        <Stack.Home name="Perfil" component={Perfil}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 
 };
