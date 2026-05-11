01a9d2673a6af6e1a1a34d5a7065b77760fc115c
LucasRafa22 <lucassolimene22@gmail.com>
2026-05-11 16:32:39 -0300

    feat: reorganized routes

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from "./src/screens/home";
-import CadastroPet from "./src/screens/CadastroPet";
-import Lembretes from "./src/screens/Lembretes";
-import ListaPets from "./src/screens/ListaPets";
-import Perfil from "./src/screens/Perfil";
+import Home from "./src/screens/Home";
+import CadastroPet from "./src/screens/CadastroPetScreen";
+import ListaPets from "./src/screens/ListaPetsScreen";
+import Lembretes from "./src/screens/LembretesScreen";
+import Perfil from "./src/screens/PerfilScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Home name="Home" component={Home}/>
-        <Stack.Home name="CadastroPet" component={CadastroPet}/>
-        <Stack.Home name="Lembretes" component={Lembretes}/>
-        <Stack.Home name="ListaPets" component={ListaPets}/>
-        <Stack.Home name="Perfil" component={Perfil}/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="CadastroPet" component={CadastroPet} />
+        <Stack.Screen name="ListaPets" component={ListaPets} />
+        <Stack.Screen name="Lembretes" component={Lembretes} />
+        <Stack.Screen name="Perfil" component={Perfil} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
-};
-
+}
