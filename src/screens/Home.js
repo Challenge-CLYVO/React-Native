7616d8fd2942c67979db3a68965af8a040049f56
LucasRafa22 <lucassolimene22@gmail.com>
2026-05-11 16:33:32 -0300

    feature: reorganized home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,29 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button } from 'react-native';
 
-export default function Home({navigation}){
-    return(
-        <View>
-            <Text>Pet Care</Text>
-            <Button title="Cadastrar Pet" onPress={() => navigation.navigate('Cadastro')} />
-            <Button title="Lista de Pets" onPress={() => navigation.navigate('Lista')} />
-            <Button title="Lembretes" onPress={() => navigation.navigate('Lembretes')} />
-            <Button title="Perfil" onPress={() => navigation.navigate('Perfil')} />
-        </View>
-    )
+export default function Home({ navigation }) {
+  return (
+    <View>
+      <Text>Home</Text>
+
+      <Button
+        title="Cadastro Pet"
+        onPress={() => navigation.navigate('CadastroPet')}
+      />
+
+      <Button
+        title="Lista Pets"
+        onPress={() => navigation.navigate('ListaPets')}
+      />
+
+      <Button
+        title="Lembretes"
+        onPress={() => navigation.navigate('Lembretes')}
+      />
+
+      <Button
+        title="Perfil"
+        onPress={() => navigation.navigate('Perfil')}
+      />
+    </View>
+  );
 }
