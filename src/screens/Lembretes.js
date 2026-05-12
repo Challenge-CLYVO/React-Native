a15a5384c6123f8a61960fd376dce56c2c0cefa0
LucasRafa22 <lucassolimene22@gmail.com>
2026-05-12 07:46:28 -0300

    feat: style lembretes

--- src/screens/Lembretes.js
+++ src/screens/Lembretes.js
@@ -1,9 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function Lembretes() {
+export default function LembretesScreen() {
   return (
-    <View>
-      <Text>Lembretes</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Lembretes</Text>
+
+      <View style={styles.card}>
+        <Text>Vacina - 10/05</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text>Consulta - 20/05</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#f2f6ff', padding: 20 },
+  title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20 },
+
+  card: {
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10
+  }
+});
